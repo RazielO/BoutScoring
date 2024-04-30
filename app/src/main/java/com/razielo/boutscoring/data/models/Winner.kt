447dbeb9988025054d31277fb933203e2ece4e33
RazielO <oraziel_a@hotmail.com>
2024-04-29 18:48:28 -0600

    Enum Winner added a companion objet to get the value from its display name

--- app/src/main/java/com/razielo/boutscoring/data/models/Winner.kt
+++ app/src/main/java/com/razielo/boutscoring/data/models/Winner.kt
@@ -1,8 +1,13 @@
 package com.razielo.boutscoring.data.models
 
-enum class Winner {
-    BLUE_CORNER,
-    RED_CORNER,
-    TIE,
-    NO_RESULT
+enum class Winner(val display: String) {
+    RED_CORNER("Red Corner"),
+    BLUE_CORNER("Blue Corner"),
+    DRAW("Draw"),
+    NO_RESULT("No Result");
+
+    companion object {
+        private val map = entries.associateBy { it.display }
+        infix fun from(display: String) = map[display]
+    }
 }

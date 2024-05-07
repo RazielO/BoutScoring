818b5e6759393de6bd6b372591f806fed0be572a
RazielO <oraziel_a@hotmail.com>
2024-05-06 23:04:07 -0600

    Room 2.3 and higher includes a default type converter for persisting enums

--- app/src/main/java/com/razielo/boutscoring/data/BoutDatabase.kt
+++ app/src/main/java/com/razielo/boutscoring/data/BoutDatabase.kt
@@ -5,22 +5,12 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.razielo.boutscoring.data.converters.DrawMethodConverter
-import com.razielo.boutscoring.data.converters.NoResultMethodConverter
 import com.razielo.boutscoring.data.converters.ScoresConverter
-import com.razielo.boutscoring.data.converters.WinMethodConverter
-import com.razielo.boutscoring.data.converters.WinnerConverter
 import com.razielo.boutscoring.data.dao.BoutDao
 import com.razielo.boutscoring.data.models.Bout
 
 @Database(entities = [Bout::class], version = 1, exportSchema = false)
-@TypeConverters(
-    DrawMethodConverter::class,
-    NoResultMethodConverter::class,
-    ScoresConverter::class,
-    WinMethodConverter::class,
-    WinnerConverter::class
-)
+@TypeConverters(ScoresConverter::class)
 abstract class BoutDatabase : RoomDatabase() {
     abstract fun boutDao(): BoutDao
 

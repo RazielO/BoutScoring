48f6a2e0188463b7d991a2a976759792e649a47c
RazielO <oraziel_a@hotmail.com>
2024-05-12 00:01:59 -0600

    Removed unnecessary pacelize

--- app/src/main/java/com/razielo/boutscoring/data/models/Bout.kt
+++ app/src/main/java/com/razielo/boutscoring/data/models/Bout.kt
@@ -1,14 +1,11 @@
 package com.razielo.boutscoring.data.models
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 import java.util.Date
 import java.util.UUID
 
-@Parcelize
 @Entity
 data class Bout(
     @PrimaryKey @ColumnInfo(name = "bout_id") val id: String = UUID.randomUUID().toString(),
@@ -22,4 +19,4 @@ data class Bout(
     @ColumnInfo(name = "no_result_method") val noResultMethod: NoResultMethod? = null,
     @ColumnInfo(name = "created_at") val createdAt: Date = Date(),
     @ColumnInfo(name = "updated_at") val updatedAt: Date = Date()
-) : Parcelable
+)
--- app/src/main/java/com/razielo/boutscoring/data/models/Fighter.kt
+++ app/src/main/java/com/razielo/boutscoring/data/models/Fighter.kt
@@ -1,14 +1,11 @@
 package com.razielo.boutscoring.data.models
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 @Entity
 data class Fighter(
     @PrimaryKey @ColumnInfo(name = "full_name") val fullName: String,
     @ColumnInfo(name = "display_name") val displayName: String,
-) : Parcelable
+)

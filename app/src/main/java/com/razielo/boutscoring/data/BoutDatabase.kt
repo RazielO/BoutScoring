17e9e1aced3859ed2e001420def9360a8a104d6f
RazielO <oraziel_a@hotmail.com>
2025-12-15 13:29:17 -0600

    Removed unnecessary permissions

--- app/src/main/java/com/razielo/boutscoring/data/BoutDatabase.kt
+++ app/src/main/java/com/razielo/boutscoring/data/BoutDatabase.kt
@@ -1,10 +1,6 @@
 package com.razielo.boutscoring.data
 
-import android.content.ContentValues
 import android.content.Context
-import android.os.Environment
-import android.provider.MediaStore
-import android.widget.Toast
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -18,9 +14,6 @@ import com.razielo.boutscoring.data.models.Bout
 import com.razielo.boutscoring.data.models.BoutInfo
 import com.razielo.boutscoring.data.models.Fighter
 import com.razielo.boutscoring.data.models.relations.BoutFighterCrossRef
-import java.io.IOException
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 @Database(
     entities = [Bout::class, Fighter::class, BoutFighterCrossRef::class, BoutInfo::class],
@@ -45,47 +38,10 @@ abstract class BoutDatabase : RoomDatabase() {
         fun getDatabase(context: Context): BoutDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    BoutDatabase::class.java,
-                    DB_NAME
+                    context.applicationContext, BoutDatabase::class.java, DB_NAME
                 ).build()
                 INSTANCE = instance
                 instance
-            }
-        }
-
-        fun exportDatabase(context: Context) {
-            val current = LocalDateTime.now()
-            val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-            val today = current.format(formatter)
-
-            val dbFile = context.getDatabasePath(DB_NAME)
-
-            val contentResolver = context.contentResolver
-            val downloadsUri = MediaStore.Downloads.EXTERNAL_CONTENT_URI
-            val contentValues = ContentValues().apply {
-                put(MediaStore.Downloads.DISPLAY_NAME, "${today}-bouts.db")
-                put(MediaStore.Downloads.MIME_TYPE, MIME_TYPE)
-                put(MediaStore.Downloads.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
-            }
-
-            val uri = contentResolver.insert(downloadsUri, contentValues)
-
-            try {
-                uri?.let {
-                    contentResolver.openOutputStream(it)?.use { outputStream ->
-                        dbFile.inputStream().use { inputStream ->
-                            inputStream.copyTo(outputStream)
-                        }
-                    }
-                    Toast.makeText(context, "Database Exported Successfully", Toast.LENGTH_SHORT)
-                        .show()
-                }.run {
-                    Toast.makeText(context, "Failed to Export Database", Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-                Toast.makeText(context, "Failed to Export Database", Toast.LENGTH_SHORT).show()
             }
         }
     }
